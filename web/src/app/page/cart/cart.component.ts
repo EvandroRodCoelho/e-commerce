01344c3a87a409a80d7aef19a8e2dfc91fe49b42
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-10-07 08:21:40 -0300

    feat:add count of prices

--- web/src/app/page/cart/cart.component.ts
+++ web/src/app/page/cart/cart.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+interface Product {
+  name: string;
+  price: number;
+  quantity: number;
+  brand: string;
+  selected: boolean;
+}
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+
+
+export class CartComponent implements OnInit {
+ count: number = 0;
+
+ totalValue: number = 0;
+
+
+  @Output() countChanged = new EventEmitter<number>();
+  products: Product[] = [
+    { name: 'Product 1', price: 100, quantity: 10, brand:"Marca 1",selected:false },
+    { name: 'Product 2', price: 150, quantity: 0, brand:"Marca 2",selected:false },
+  ];
+
+  calculateTotal(): number {
+    let total = 0;
+    for (const product of this.products) {
+      total += product.price * product.quantity;
+    }
+    return total;
+  }
+
+  increment(product: Product) {
+    product.quantity++;
+  }
+
+  decrement(product: Product) {
+    if (product.quantity > 0) {
+      product.quantity--;
+    }
+  }
+  checkboxChanged(product: Product) {
+
+    if (product.quantity !== 0) {
+      product.selected = !product.selected;
+    }
+  }
+  ngOnInit() {
+    this.calculateTotal();
+  }
+  completePurchase() {
+    const selectedProducts = this.products.filter(product => product.selected);
+
+
+    console.log(selectedProducts);
+  }
 
 }
