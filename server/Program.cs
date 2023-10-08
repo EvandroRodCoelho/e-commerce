e450f5f92d22cf426bc18dac761a41ec60e16fac
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-10-08 18:07:09 -0300

    Feat: Add CORS to server

--- server/Program.cs
+++ server/Program.cs
@@ -2,10 +2,19 @@ using System.Net;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
-
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(MyAllowSpecificOrigins, builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
 // Adicione a configuração do banco de dados ao projeto
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer("Server=EVANDRO\\SQLEXPRESS2023;Database=ecommerce;User Id=sa;Password=1234;TrustServerCertificate=True"));
@@ -23,5 +32,5 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
-
+app.UseCors(MyAllowSpecificOrigins);
 app.Run();
