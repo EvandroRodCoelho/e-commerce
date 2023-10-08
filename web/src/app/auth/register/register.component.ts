ebc92b2f839884d7d3ce409a0369dacb496f865f
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-10-08 18:25:04 -0300

    Feat: Register

--- web/src/app/auth/register/register.component.ts
+++ web/src/app/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  login= '';
+  password= '';
+  confirmPassword= '';
 
+  showMessage=false;
+  message = '';
+
+  constructor (private router: Router){}
+
+  async register() {
+    if (!this.login || !this.password || !this.confirmPassword) {
+      this.showMessage = true;
+      this.message = 'Por favor, preencha todos os campos.';
+      return;
+    }
+
+    if (this.password !== this.confirmPassword) {
+      this.showMessage = true;
+      this.message = 'As senhas não coincidem.';
+      return;
+    }
+
+    const userCredentials = { login: this.login, password: this.password };
+    try {
+      const response = await axios.post('http://localhost:5126/api/users', userCredentials);
+
+      if (response.status === 201) {
+        this.showMessage = true;
+        this.message = 'Usuário registrado com sucesso.';
+
+        this.router.navigate(['/login']);
+      } else {
+        console.error('Erro ao registrar usuário:', response);
+      }
+
+    } catch (error) {
+      if ((error as AxiosError).response) {
+        const axiosError = error as AxiosError;
+        const response = axiosError.response as AxiosResponse;
+
+        if (response.status == 400) {
+          this.showMessage = true;
+          this.message = 'Erro ao criar usuário: Verifique os dados inseridos.';
+        } else {
+          this.showMessage = true;
+          this.message = 'Erro inesperado. Tente novamente mais tarde.';
+        }
+      } else {
+        console.error('Erro na requisição:', error);
+        this.showMessage = true;
+        this.message = 'Erro inesperado. Tente novamente mais tarde.';
+      }
+    }
+  }
 }
