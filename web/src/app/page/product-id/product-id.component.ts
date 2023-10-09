a5f841d5c812d0effb5ce7a379f3579cf36ec288
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-10-09 09:52:39 -0300

    Feat: Get id product from url

--- web/src/app/page/product-id/product-id.component.ts
+++ web/src/app/page/product-id/product-id.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
-
+import { ProductsProps } from 'src/types/Products';
+import { ActivatedRoute, Router } from '@angular/router';
+import axios from 'axios';
 @Component({
   selector: 'app-product-id',
   templateUrl: './product-id.component.html',
   styleUrls: ['./product-id.component.css']
 })
 export class ProductIdComponent {
+
+  product:ProductsProps= {
+    brand:'',
+    description:'',
+    id:0,
+    name:'',
+    price:0,
+    quantity:0,
+    technical_specification:''
+  };
+
+  constructor(private route: ActivatedRoute,private router: Router) { }
+
+  async ngOnInit() {
+    const idParam = this.route.snapshot.paramMap.get("id");
+    this.product.id = idParam ? parseInt(idParam, 10) : undefined;
+
+    if(!this.product.id) {
+      this.router.navigate(['/']);
+    }
+
+    const response = await  axios.get(`http://localhost:5126/api/products/${this.product.id}`);
+
+    if(!response.data) {
+      this.router.navigate(['/']);
+    }
+    this.product = response.data;
+
+    console.log(this.product);
+  }
 
 }
