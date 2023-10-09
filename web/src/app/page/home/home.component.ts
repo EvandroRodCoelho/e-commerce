625e80e5d3c31ad2054e64ed5465debea6814df1
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-10-09 10:17:27 -0300

    Feat: get products

--- web/src/app/page/home/home.component.ts
+++ web/src/app/page/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
+import { ProductsProps } from 'src/types/Products';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  products:ProductsProps[] = [];
+  async getProducts() {
+    const response = await axios.get("http://localhost:5126/api/products");
 
+    console.log(response)
+    this.products = response.data;
+
+    this.products = this.products.filter(product => product.quantity > 0 );
+
+    console.log(this.products);
+  }
+  ngOnInit() {
+    this.getProducts()
+  }
 }
---
+++ web/src/types/Products.ts
@@ -0,0 +1,10 @@
+export interface ProductsProps {
+  id?:number;
+  name: string;
+  price: number;
+  description: string;
+  quantity:number;
+
+  technical_specification:string;
+  brand:string;
+}
