43b7dc8e7d7c5ece14471a5db2fe5192283e8662
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-10-11 12:36:02 -0300

    refactor: change type of att TotalPrice

--- server/Models/OrderItems.cs
+++ server/Models/OrderItems.cs
@@ -2,7 +2,7 @@ public class OrderItem
 {
     public int Id { get; set; }
     public DateTime CreatedAt { get; set; }
-    public double TotalPrice { get; set; }
+    public float TotalPrice { get; set; }
     public int ShoppingCartItemId { get; set; }
 }
 
